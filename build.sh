b0e5de7754843edcada9260783ff0995fa4d5e82
hairay <hairay@gmail.com>
2019-09-18 08:41:42 +0800

    build bin file

--- build.sh
+++ build.sh
@@ -2,20 +2,20 @@
 
 mkdir ./linux
 make clean
-CROSS_COMPILE=aarch64-linux-gnu- make
+CROSS_COMPILE=aarch64-linux-gnu- make -j4
 mv am3xtest ./linux/am3xtest-arm64
 
 make clean
-CROSS_COMPILE=arm-linux-gnueabihf- make
+CROSS_COMPILE=arm-linux-gnueabihf- make -j4
 mv am3xtest ./linux/am3xtest-arm32
 
 
 make clean
-CROSS_COMPILE=mips64el-linux-gnuabi64- make
+CROSS_COMPILE=mips64el-linux-gnuabi64- make -j4
 mv am3xtest ./linux/am3xtest-mips64
 
 make clean
-CROSS_COMPILE=x86_64-linux-gnu- make
+CROSS_COMPILE=x86_64-linux-gnu- make -j4
 mv am3xtest ./linux/am3xtest-x64
 
 make clean
